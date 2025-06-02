42f4f1036ddf18dac3d97d41b33b7f6c2b451d13
cdslz <cds11@aluno.ifal.edu.br>
2025-06-02 15:30:49 -0300

    tela plano de fundo mascote

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+
 import 'package:flutter/material.dart';
+import 'package:mais_folego/tela_mascote.dart';
 
 void main() {
-print("mais folego");
+  runApp(const MaterialApp(home: mascote()),
+  );
 }
+
+
+
 
 
---
+++ lib/tela_mascote.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class mascote extends StatefulWidget {
+  const mascote({super.key});
+
+  @override
+  State<mascote> createState() => _mascoteState();
+}
+
+class _mascoteState extends State<mascote> {
+  @override
+  Widget build(BuildContext context) {
+    return buildbody();
+  }
+
+  buildbody() {
+    String a = 'https://s3.amazonaws.com/media.wikiaves.com.br/images/6255/5526892_beb29b4d03b0fa7762b2e2ab11d3e753.jpg';
+    String plano_fundo = 'https://img.freepik.com/fotos-premium/fundo-da-natureza_35658-136.jpg?semt=ais_hybrid&w=740';
+    return Padding(
+      padding: const EdgeInsets.all(0),
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(plano_fundo,
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Container(width: 1000,height: 70, color: Colors.blueAccent,child: Row(children: [Icon(Icons.currency_bitcoin)],
+          ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  buildbody2() {
+    return Stack(
+      children: [
+        Positioned(
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0,
+          child: Image.network(
+            'https://img.freepik.com/fotos-premium/fundo-da-natureza_35658-136.jpg?semt=ais_hybrid&w=740',
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        Scaffold(backgroundColor: Colors.transparent, body: Text('TESTE')),
+      ],
+    );
+  }
+}
